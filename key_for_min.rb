04217056f9b429cc13ba4d2106614f814fff9379
Rwanda S <rwanda.ace@gmail.com>
2020-03-14 18:59:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+ if !name_hash.empty?
+   
+ placeholder1 = name_hash.collect do |key, value|
+    key
+  end
+  placeholder2 = name_hash[placeholder1[0]]
+  name_hash.each do |key, value|
+    if placeholder2 > value
+      placeholder2 = value
+      placeholder1[0] = key
+    end
+  end
+   placeholder1[0]
+   
+   
+   
+else
+return nil
+end
 
 end
