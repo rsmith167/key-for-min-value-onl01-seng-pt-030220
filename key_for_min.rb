4bc7cd46bf047c60267ff97d0c5e47f621533fdb
Rwanda S <rwanda.ace@gmail.com>
2020-03-14 18:58:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,10 @@
 def key_for_min_value(name_hash)
  if !name_hash.empty?
    
-   placeholder1 = []
-   placeholder2 = 0
-   placeholder3 = :temp
  placeholder1 = name_hash.collect do |key, value|
     key
   end
   placeholder2 = name_hash[placeholder1[0]]
-  placeholder1 = name_hash.collect do |key, value|
-    key
-  end
   name_hash.each do |key, value|
     if placeholder2 > value
       placeholder2 = value
